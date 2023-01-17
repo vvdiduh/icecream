7fe9d869c8c1ae0b1a9faee3a813327c900df23f
vvdiduh <57592716+vvdiduh@users.noreply.github.com>
2023-01-17 17:22:48 +0000

    Deploying to gh-pages from @ vvdiduh/icecream@4d9024bda69413396933ae29c3a33668597e2e33 🚀

---
+++ index.d8a25f8c.js
@@ -0,0 +1,2 @@
+!function(){var e={openMobileBtn:document.querySelector(".mobile-open-btn"),closeMobileBtn:document.querySelector(".mobile-close-btn"),mobile:document.querySelector(".mobile-menu")};function o(){e.mobile.classList.toggle("is-hidden")}e.openMobileBtn.addEventListener("click",o),e.closeMobileBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.d8a25f8c.js.map
